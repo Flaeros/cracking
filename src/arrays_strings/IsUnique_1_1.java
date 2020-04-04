7bf7b4b5d204832f7c8a2eb53508076aa93269b7
Aleksandr Birin <sza1@ya.ru>
2020-04-04 20:00:32 +0400

    1.1 is unique string

---
+++ src/arrays_strings/IsUnique_1_1.java
@@ -0,0 +1,40 @@
+package arrays_strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * If string has all unique characters
+ */
+public class IsUnique_1_1 {
+
+
+    public static void main(String[] args) {
+        //System.out.println(Arrays.toString(new IsUnique_1_1().createTargetArray(new int[]{0,1,2,3,4}, new int[] {0,1,2,2,1})));
+        System.out.println(new IsUnique_1_1().isUnique("abcdb"));
+        System.out.println(new IsUnique_1_1().isUnique("abcde"));
+
+        System.out.println(new IsUnique_1_1().isUniqueInPlace("abcdb"));
+        System.out.println(new IsUnique_1_1().isUniqueInPlace("abcde"));
+    }
+
+    public boolean isUnique(String s) {
+        Map<Character, Boolean> uniqueChars = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            uniqueChars.merge(s.charAt(i), true, (a, b) -> false);
+        }
+        return uniqueChars.values().stream().reduce((a, b) -> a & b).get();
+    }
+
+    public boolean isUniqueInPlace(String s) {
+        //imagine in-place sort
+        String sorted = s.chars().sorted().collect(StringBuilder::new,
+                StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        for (int i = 0; i < sorted.length() - 1; i++) {
+            if (sorted.charAt(i) == sorted.charAt(i + 1))
+                return false;
+        }
+        return true;
+    }
+}
