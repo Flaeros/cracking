32cf0ef2aa188689d881d520870d1557a5362879
Aleksandr Birin <sza1@ya.ru>
2020-04-06 10:58:04 +0400

    1.5 one edit away array'y

--- src/arrays_strings/OneEditAway_1_5.java
+++ src/arrays_strings/OneEditAway_1_5.java
@@ -7,12 +7,56 @@ public class OneEditAway_1_5 {
     public static void main(String[] args) {
 
         System.out.println(new OneEditAway_1_5().oneEditAway("pale", "ple"));
-        System.out.println(new OneEditAway_1_5().oneEditAway("pales", "pale"));
+        System.out.println(new OneEditAway_1_5().oneEditAway("pale", "pales"));
         System.out.println(new OneEditAway_1_5().oneEditAway("pale", "bale"));
         System.out.println(new OneEditAway_1_5().oneEditAway("pale", "bake"));
+        System.out.println("- - - - -");
+        System.out.println(new OneEditAway_1_5().oneEditAwayBit("pale", "ple"));
+        System.out.println(new OneEditAway_1_5().oneEditAwayBit("pale", "pales"));
+        System.out.println(new OneEditAway_1_5().oneEditAwayBit("pale", "bale"));
+        System.out.println(new OneEditAway_1_5().oneEditAwayBit("pale", "bake"));
     }
 
     public boolean oneEditAway(String one, String other) {
+        if (one.length() == other.length())
+            return checkReplaced(one, other);
+        if (one.length() > other.length())
+            return checkAdded(one, other);
+        if (other.length() > one.length())
+            return checkAdded(other, one);
+        return false;
+    }
+
+    private boolean checkAdded(String one, String other) {
+        int onePos = 0;
+        int otherPos = 0;
+
+        while (onePos < one.length() && otherPos < other.length()) {
+            if (one.charAt(onePos) != other.charAt(otherPos)) {
+                if (onePos != otherPos) {
+                    return false;
+                }
+            } else {
+                otherPos++;
+            }
+            onePos++;
+        }
+
+        return true;
+    }
+
+    private boolean checkReplaced(String one, String other) {
+        boolean oneEdit = false;
+        for (int i = 0; i < one.length(); i++) {
+            if (one.charAt(i) != other.charAt(i))
+                if (oneEdit)
+                    return false;
+            oneEdit = true;
+        }
+        return oneEdit;
+    }
+
+    public boolean oneEditAwayBit(String one, String other) {
         int bitOne = 0;
         int bitOther = 0;
 
@@ -32,7 +76,7 @@ public class OneEditAway_1_5 {
         boolean oneCharDiff = Math.abs(one.length() - other.length()) == 1;
         if (oneCharDiff && Integer.bitCount(diff) == 1)
             return true;
-        if (Integer.bitCount(diff)  == 2)
+        if (Integer.bitCount(diff) == 2)
             return true;
         return false;
     }
