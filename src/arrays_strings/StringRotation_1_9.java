18d5b80938afaa5b78a083c385ba1bb73794b7ac
Aleksandr Birin <sza1@ya.ru>
2020-04-06 14:11:32 +0400

    1.9 string rotation

---
+++ src/arrays_strings/StringRotation_1_9.java
@@ -0,0 +1,16 @@
+package arrays_strings;
+
+public class StringRotation_1_9 {
+    public static void main(String[] args) {
+        System.out.println(new StringRotation_1_9().isStringRotation("waterbottle", "erbottlewat"));
+    }
+
+    public boolean isStringRotation(String one, String other) {
+        return isSubstring(one, other + other);
+    }
+
+    public boolean isSubstring(String sub, String string) {
+        int i = string.indexOf(sub);
+        return i != -1;
+    }
+}
