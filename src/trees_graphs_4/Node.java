4c21899b5c58330f32ecd4a9373379c744bf547c
Aleksandr Birin <sza1@ya.ru>
2020-04-15 21:31:32 +0400

    route two nodes 4_1

--- src/trees_graphs_4/Node.java
+++ src/trees_graphs_4/Node.java
@@ -3,6 +3,7 @@ package trees_graphs_4;
 public class Node {
     public String name;
     public Node[] children;
+    public boolean visited;
 
     public Node(int name) {
         this.name = String.valueOf(name);
--- src/trees_graphs_4/RouteTwoNodes_4_1.java
+++ src/trees_graphs_4/RouteTwoNodes_4_1.java
@@ -1,5 +1,8 @@
 package trees_graphs_4;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Route Between Nodes: Given a directed graph,
  * design an algorithm to find out whether there is a
@@ -13,9 +16,37 @@ public class RouteTwoNodes_4_1 {
                 {1, 0, 0}});
         System.out.println(new RouteTwoNodes_4_1().search(graph, graph.nodes[0], graph.nodes[2]));
 
+        graph = Graph.graph(new int[][]{
+                {0, 1, 0, 0},
+                {0, 0, 1, 0},
+                {1, 0, 0, 0},
+                {0, 0, 0, 0}});
+        System.out.println(new RouteTwoNodes_4_1().search(graph, graph.nodes[0], graph.nodes[3]));
+
     }
 
     boolean search(Graph g, Node start, Node end) {
-        return true;
+        if (start == end)
+            return true;
+
+        Queue<Node> q = new LinkedList<>();
+
+        q.offer(start);
+        start.visited = true;
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+
+            for (Node child : node.children) {
+                if (child.visited)
+                    continue;
+                if (child == end)
+                    return true;
+
+                child.visited = true;
+                q.offer(child);
+            }
+        }
+        return false;
     }
 }
