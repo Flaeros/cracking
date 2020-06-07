fd11ec90dfff41b4f52a8e9469b20e659acdbf26
Aleksandr Birin <sza1@ya.ru>
2020-06-07 13:34:16 +0400

    coins readabbility

--- src/recursion_dynamic_8/ChangeCoines_8_11.java
+++ src/recursion_dynamic_8/ChangeCoines_8_11.java
@@ -1,16 +1,20 @@
 package recursion_dynamic_8;
 
+import static recursion_dynamic_8.RobotInAGrid_8_2.printDim;
+
 public class ChangeCoines_8_11 {
     public static void main(String[] args) {
-        System.out.println(new ChangeCoines_8_11().makeChange(4));
-        System.out.println(new ChangeCoines_8_11().makeChange(16));
-        System.out.println(new ChangeCoines_8_11().makeChange(100));
+        System.out.println(new ChangeCoines_8_11().makeChange(4, new int[]{25, 10, 5, 1}));
+        System.out.println(new ChangeCoines_8_11().makeChange(16, new int[]{25, 10, 5, 1}));
+        System.out.println(new ChangeCoines_8_11().makeChange(100, new int[]{25, 10, 5, 1}));
+        System.out.println(new ChangeCoines_8_11().makeChange(5, new int[]{5, 2, 1}));
     }
 
-    int makeChange(int amount) {
-        int[] denoms = new int[]{25, 10, 5, 1};
+    int makeChange(int amount, int[] denoms) {
         int[][] memo = new int[amount + 1][denoms.length];
-        return waysToChange(amount, denoms, memo, 0);
+        int waysToChange = waysToChange(amount, denoms, memo, 0);
+        System.out.println(printDim(memo));
+        return waysToChange;
     }
 
     int waysToChange(int amount, int[] denoms, int[][] memo, int index) {
@@ -27,6 +31,8 @@ public class ChangeCoines_8_11 {
             int remaining = amount - i * denomAmount;
             ways += waysToChange(remaining, denoms, memo, index + 1);
         }
+
+        memo[amount][index] = ways;
 
         return ways;
     }
